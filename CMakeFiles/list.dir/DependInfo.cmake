695367f7ca9f286365ab14050d2b34171c93cb13
valentinod3m0n <therolus@gmail.com>
2018-11-13 14:12:00 +0100

    Changed to CMake

---
+++ CMakeFiles/list.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/valentinod3m0n/Code/list-c-/list.cc" "/home/valentinod3m0n/Code/list-c-/CMakeFiles/list.dir/list.cc.o"
+  "/home/valentinod3m0n/Code/list-c-/list_node.cc" "/home/valentinod3m0n/Code/list-c-/CMakeFiles/list.dir/list_node.cc.o"
+  "/home/valentinod3m0n/Code/list-c-/main.cc" "/home/valentinod3m0n/Code/list-c-/CMakeFiles/list.dir/main.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/list.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/list.dir/list_node.cc.o"
+  "CMakeFiles/list.dir/list.cc.o"
+  "CMakeFiles/list.dir/main.cc.o"
+  "list.pdb"
+  "list"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/list.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
